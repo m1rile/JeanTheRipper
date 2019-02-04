8e3a89bb193a69d7ae66bdea66829ed5a43cafb7
Low_battery <marile.lin@epita.fr>
2019-02-04 19:05:08 +0100

    test

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//FR"
+  "http://jeantheripper.ml/">
+<html xmlns="http://jeantheripper.ml/"
+  xml:lang="fr" lang="fr">
+<head>
+</head><body>
+  <h1><?php include('index.html'); ?></h1>
+</body>
+</html>
